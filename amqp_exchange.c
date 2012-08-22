48478db780688c4cb2365feadad9b7fd9b392392
Bernhard K. Weisshuhn <bkw@codingforce.com>
2012-08-22 17:44:41 +0200

    fix reporting of errors during publish()

--- amqp_exchange.c
+++ amqp_exchange.c
@@ -789,11 +789,10 @@ PHP_METHOD(amqp_exchange_class, publish)
 	signal(SIGPIPE, old_handler);
 
 	/* handle any errors that occured outside of signals */
-	if (r) {
+	if (r < 0) {
 		char str[256];
 		char ** pstr = (char **) &str;
-		res = (amqp_rpc_reply_t)amqp_get_rpc_reply(connection->connection_resource->connection_state); 
-		amqp_error(res, pstr);
+		spprintf(pstr, 0, "Socket error: %s", amqp_error_string(-r));
 		zend_throw_exception(amqp_exchange_exception_class_entry, *pstr, 0 TSRMLS_CC);
 		return;
 	}
